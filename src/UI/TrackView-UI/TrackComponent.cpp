98f2d2a31606865e601735bca42987c076d223b6
Pablo Arancibia-Bazan <ikerarancibia@hotmail.com>
2025-04-09 19:01:44 -0600

    made edit component

--- src/UI/TrackView-UI/TrackComponent.cpp
+++ src/UI/TrackView-UI/TrackComponent.cpp
@@ -8,24 +8,14 @@
 
 using namespace juce;
 TrackComponent::TrackComponent() {
-    addAndMakeVisible(trackHeader);
     addAndMakeVisible(trackClip);
 }
 
 TrackComponent::~TrackComponent() {
-    trackHeader.removeListener(this);
 };
 
 void TrackComponent::paint(juce::Graphics &g) {
     g.fillAll(juce::Colours::grey);
-
-    // this draws a line right after the track header
-    // separating the clips from the track header
-    if (trackHeader.getRight() > 0) {
-        const int lineX = trackHeader.getRight() + 5.0f;
-        g.setColour(juce::Colours::black);
-        g.drawLine(lineX, 0, lineX, getHeight(), 2.0f);
-    }
     // TODO: select tracks, Take a look at example from Tracktion
 }
 
@@ -39,12 +29,6 @@ void TrackComponent::resized() {
     //define the margins
     const float margin = 5.0f;
     const FlexItem::Margin itemMargin(margin);
-
-    // Track Header
-    mainFB.items.add(FlexItem(trackHeader)
-        .withWidth(100)
-        .withHeight(getHeight() - itemMargin.top - itemMargin.bottom)
-        .withMargin(itemMargin));
 
     mainFB.items.add(FlexItem(trackClip)
         .withWidth(300)
