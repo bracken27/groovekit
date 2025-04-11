2f07440b222a10212f84cee1c1c2cf4ba9c67ec2
BurmeseJoe323 <josephkrockwell@duck.com>
2025-04-11 11:37:49 -0600

    Added the rest of the needed components

---
+++ src/UI/PopupWindows/PianoRollComponents/GridStyleSheet.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Joseph Rockwell on 4/11/25.
+//
+
+#include "GridStyleSheet.h"
+
+GridStyleSheet::GridStyleSheet() {
+    drawMIDINum = false;
+    drawMIDINoteStr = false;
+    drawVelocity = true;
+    disableEditing = false;
+}
+
+bool GridStyleSheet::getDrawMIDINum() {
+    return drawMIDINum;
+}
+
+bool GridStyleSheet::getDrawMIDINoteStr() {
+    return drawMIDINoteStr;
+}
+
+bool GridStyleSheet::getDrawVelocity() {
+    return drawVelocity;
+}
