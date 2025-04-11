ec81090cdcbc99d2195a3c5d27ce3e8128da4ed4
BurmeseJoe323 <josephkrockwell@duck.com>
2025-04-11 11:37:49 -0600

    Added the rest of the needed components

---
+++ src/UI/PopupWindows/PianoRollComponents/GridControlPanel.h
@@ -0,0 +1,38 @@
+//
+// Created by Joseph Rockwell on 4/11/25.
+//
+
+#ifndef GRIDCONTROLPANEL_H
+#define GRIDCONTROLPANEL_H
+
+#include "GridStyleSheet.h"
+#include "NoteGridComponent.h"
+#include "PConstants.h"
+
+class GridControlPanel : public juce::Component {
+public:
+    GridControlPanel (NoteGridComponent & component, GridStyleSheet & styleSheet);
+    ~GridControlPanel ();
+
+    void resized () override;
+    void paint (juce::Graphics & g) override;
+
+    void renderSequence ();
+    std::function<void(int pixelsPerBar, int noteHeight)> configureGrid;
+
+    void setQuantisation (PRE::eQuantisationValue value);
+private:
+
+    NoteGridComponent & noteGrid;
+    GridStyleSheet & styleSheet;
+
+    juce::Slider noteCompHeight, pixelsPerBar;
+
+    juce::TextButton render;
+    juce::ToggleButton drawMIDINotes, drawMIDIText, drawVelocity;
+
+    juce::ComboBox quantisationVlaue;
+};
+
+
+#endif //GRIDCONTROLPANEL_H
