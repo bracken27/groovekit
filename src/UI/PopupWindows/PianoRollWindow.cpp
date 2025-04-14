c0fd3fc1f8b87b005a2c85e33ebffa8194d21ab1
BurmeseJoe323 <josephkrockwell@duck.com>
2025-04-14 15:56:31 -0600

    Fixed pop-up window to size properly on opening

--- src/UI/PopupWindows/PianoRollWindow.cpp
+++ src/UI/PopupWindows/PianoRollWindow.cpp
@@ -6,14 +6,14 @@
 
 PianoRollWindow::PianoRollWindow() : DocumentWindow("Piano Roll Editor", juce::Colours::darkblue,
                                                     DocumentWindow::closeButton, true) {
-    // TODO: get window to size properly on opening
-    centreWithSize(800, 600);
     setUsingNativeTitleBar(true);
-    setResizable(true, false);
     pianoRoll = std::make_unique<PianoRollEditor>();
     pianoRoll->setup(10, 900, 20);
-    setContentOwned(pianoRoll.get(), true);
     pianoRoll->setVisible(true);
+    setContentOwned(pianoRoll.get(), true);
+
+    setResizable(true, false);
+    centreWithSize(800, 600);
 }
 
 PianoRollWindow::~PianoRollWindow() = default;
