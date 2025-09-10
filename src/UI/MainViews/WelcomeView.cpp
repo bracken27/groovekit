11101b303bb3d05e6ce7be5cfc61ec7f142d9d46
Tommy Shelburne <tshelburne02@gmail.com>
2025-09-10 11:11:00 -0600

    Default size now 800x600

--- src/UI/MainViews/WelcomeView.cpp
+++ src/UI/MainViews/WelcomeView.cpp
@@ -1,19 +1,16 @@
-//
-// Created by Joseph Rockwell on 4/8/25.
-//
-
 #include "WelcomeView.h"
-
 #include "../MainComponent.h"
+#include "TrackView/TrackEditView.h"
+#include "TutorialManager/TutorialManagerComponent.h"
 
-WelcomeView::WelcomeView(AppEngine& engine, DatabaseManager& db)
-    : appEngine(engine), databaseManager(db)
+WelcomeView::WelcomeView (AppEngine& engine, DatabaseManager& db)
+    : appEngine (engine), databaseManager (db)
 {
-    setSize(600, 400);
+    setSize (800, 600);
 
-    addAndMakeVisible(openTrackView);
-    addAndMakeVisible(openTutorialManager);
-    addAndMakeVisible(deleteUserData);
+    addAndMakeVisible (openTrackView);
+    addAndMakeVisible (openTutorialManager);
+    addAndMakeVisible (deleteUserData);
 
     openTrackView.onClick = [this]() {
         auto* trackView = new TrackEditView();
@@ -21,42 +18,42 @@ WelcomeView::WelcomeView(AppEngine& engine, DatabaseManager& db)
             currentView.reset();
             resized();
         };
-        currentView.reset(trackView);
-        addAndMakeVisible(currentView.get());
+        currentView.reset (trackView);
+        addAndMakeVisible (currentView.get());
         resized();
     };
 
     openTutorialManager.onClick = [this]() {
-        auto* tutorialManager = new TutorialManagerComponent(appEngine, databaseManager);
+        auto* tutorialManager = new TutorialManagerComponent (appEngine, databaseManager);
         tutorialManager->onBack = [this]() {
             currentView.reset();
             resized();
         };
-        currentView.reset(tutorialManager);
-        addAndMakeVisible(currentView.get());
+        currentView.reset (tutorialManager);
+        addAndMakeVisible (currentView.get());
         resized();
     };
 
     deleteUserData.onClick = [this]() {
-        databaseManager.deleteUserData("User1");
+        databaseManager.deleteUserData ("User1");
     };
 }
 
-void WelcomeView::paint(juce::Graphics& g)
+void WelcomeView::paint (juce::Graphics& g)
 {
-    g.fillAll(juce::Colours::black);
-    g.setColour(juce::Colours::white);
-    g.setFont(20.0f);
-    g.drawText("Welcome to GrooveKit!", getLocalBounds().removeFromTop(40), juce::Justification::centred, true);
+    g.fillAll (juce::Colours::black);
+    g.setColour (juce::Colours::white);
+    g.setFont (20.0f);
+    g.drawText ("Welcome to GrooveKit!", getLocalBounds().removeFromTop (40), juce::Justification::centred, true);
 }
 
 void WelcomeView::resized()
 {
-    auto area = getLocalBounds().reduced(20);
-    openTrackView.setBounds(area.removeFromBottom(40).removeFromLeft(200));
-    openTutorialManager.setBounds(area.removeFromBottom(40).removeFromLeft(200));
-    deleteUserData.setBounds(area.removeFromBottom(40).removeFromLeft(200));
+    auto area = getLocalBounds().reduced (20);
+    openTrackView.setBounds (area.removeFromBottom (40).removeFromLeft (200));
+    openTutorialManager.setBounds (area.removeFromBottom (40).removeFromLeft (200));
+    deleteUserData.setBounds (area.removeFromBottom (40).removeFromLeft (200));
 
     if (currentView)
-        currentView->setBounds(getLocalBounds().reduced(10));
+        currentView->setBounds (getLocalBounds().reduced (10));
 }
