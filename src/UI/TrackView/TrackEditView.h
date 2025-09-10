afb30162daba78e540f0cf694a66ce064c951d94
Tommy Shelburne <tshelburne02@gmail.com>
2025-09-10 00:09:17 -0600

    Cleanup

--- src/UI/TrackView/TrackEditView.h
+++ src/UI/TrackView/TrackEditView.h
@@ -1,7 +1,3 @@
-//
-// Created by Bracken Asay on 4/2/25.
-//
-
 #pragma once
 
 #include "../../AppEngine/AppEngine.h"
@@ -13,30 +9,27 @@
  * Represents the track editor view, with functionality for adding and deleting tracks.
  * Each track contains a corresponding header, footer, and a series of MIDI clips.
  */
-class TrackEditView final : public Component
+class TrackEditView final : public juce::Component
 {
 public:
     explicit TrackEditView ();
     ~TrackEditView() override;
 
     void setupButtons();
-    void paint (Graphics&) override;
+    void paint (juce::Graphics&) override;
     void resized() override;
 
     /**
-      Called when Back is pressed: should return to home screen.
-    */
+     * Called when Back is pressed: should return to home screen.
+     */
     std::function<void()> onBack;
 
 private:
     std::shared_ptr<AppEngine> appEngine;
     std::unique_ptr<TrackListComponent> trackList;
     std::unique_ptr<PianoRollWindow> pianoRollWindow;
-    Viewport viewport;
+    juce::Viewport viewport;
 
-    // Do we need this here?
-    // EditViewState &editViewState;
-
-    TextButton backButton { "Back" }, newEditButton{"New"}, playPauseButton{"Play"}, stopButton{"Stop"},
+    juce::TextButton backButton { "Back" }, newEditButton{"New"}, playPauseButton{"Play"}, stopButton{"Stop"},
             openEditButton{"Open Edit"}, newTrackButton{"New Track"}, recordButton{"Record"};
 };
