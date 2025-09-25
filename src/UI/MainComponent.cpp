7f71b402873db5cc7bc0c73028bacbc3c90cafb1
Tommy Shelburne <tshelburne02@gmail.com>
2025-09-25 08:50:03 -0600

    Remove all tutorial and database references

--- src/UI/MainComponent.cpp
+++ src/UI/MainComponent.cpp
@@ -1,60 +1,18 @@
-
-#include "TrackView/TrackEditView.h"
 #include "MainComponent.h"
-#include "InstrumentTutorial/InstrumentTutorial.h"
-#include "MainViews/WelcomeView.h"
-#include "TrackEditViewTutorial/TrackEditViewTutorial.h"
 #include "TrackView/TrackEditView.h"
 
 MainComponent::MainComponent()
 {
     setSize(1200, 800);
-    databaseManager.initialize();
-
-    view = std::make_unique<WelcomeView> (appEngine, databaseManager);
+    view = std::make_unique<TrackEditView> (appEngine);
     addAndMakeVisible (view.get());
     view->setBounds (getLocalBounds());
 }
 
 MainComponent::~MainComponent() = default;
 
-void MainComponent::showWelcomeView()
-{
-    removeAllChildren();
-    view = std::make_unique<WelcomeView>(appEngine, databaseManager);
-    addAndMakeVisible(view.get());
-    view->setBounds(getLocalBounds());
-}
-
-
-
-void MainComponent::showTrackViewTutorial()
-{
-    removeAllChildren();
-    databaseManager.addTutorial("TrackViewTutorial");
-    view = std::make_unique<TrackEditViewTutorial>(databaseManager);
-    addAndMakeVisible(view.get());
-
-    view->setBounds(getLocalBounds());
-}
-
-void MainComponent::showInstrumentTutorial() {
-    removeAllChildren();
-    databaseManager.addTutorial("InstrumentTutorial");
-    view = std::make_unique<InstrumentTutorial>(databaseManager);
-    addAndMakeVisible(view.get());
-
-    view->setBounds(getLocalBounds());
-}
-
 void MainComponent::resized()
 {
     if (view)
         view->setBounds(getLocalBounds());
-}
-
-void MainComponent::reportDatabaseSize()
-{
-    int size = databaseManager.selectCompletedTutorials("User1").size();
-    std::cout << "There are: " << size <<  " elements in the array" << std::endl;
 }
--- src/UI/MainComponent.h
+++ src/UI/MainComponent.h
@@ -3,9 +3,6 @@
 #include <juce_gui_basics/juce_gui_basics.h>
 #include "TrackView/TrackEditView.h"
 #include "../AppEngine/AppEngine.h"
-#include "TrackEditViewTutorial/TrackEditViewTutorial.h"
-#include "../DatabaseManager/DatabaseManager.h"
-#include <juce_gui_basics/juce_gui_basics.h>
 
 class MainComponent final : public juce::Component
 {
@@ -13,18 +10,9 @@ public:
     MainComponent();
     ~MainComponent() override;
 
-
-    void showWelcomeView();
-    void showTrackViewTutorial();
-    void showInstrumentTutorial();
-
     void resized() override;
-
-    void reportDatabaseSize();
 
 private:
     std::unique_ptr<Component> view;
     AppEngine appEngine;
-
-    DatabaseManager databaseManager;
 };
