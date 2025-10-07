b091782fc181e03383e05bbc05fe2ecd26da6c80
BurmeseJoe323 <josephkrockwell@duck.com>
2025-10-06 19:14:41 -0600

    Mouse cursor turns into an i-beam when hovering over the track name

--- src/UI/TrackView/TrackHeaderComponent.cpp
+++ src/UI/TrackView/TrackHeaderComponent.cpp
@@ -42,6 +42,9 @@ TrackHeaderComponent::TrackHeaderComponent()
     // Using setEditable(false, true, false) enables editing only on double-click, not single-click.
     trackNameLabel.setEditable (false, true, false);
 
+    // Use I-beam cursor when hovering over the track name to indicate text editing
+    trackNameLabel.setMouseCursor (juce::MouseCursor::IBeamCursor);
+
     setTrackType (TrackType::Instrument);
 }
 
@@ -107,7 +110,7 @@ void TrackHeaderComponent::resized()
     constexpr int buttonHeight = 25;
     const auto margin = juce::FlexItem::Margin (2, 0, 2, 0);
 
-    fb.items.add (juce::FlexItem (trackNameLabel).withHeight (22.5f));
+    fb.items.add (juce::FlexItem (trackNameLabel).withHeight (20.f));
     fb.items.add (juce::FlexItem (settingsButton).withHeight (buttonHeight).withMargin (margin));
     fb.items.add (juce::FlexItem (muteTrackButton).withHeight (buttonHeight).withMargin (margin));
     fb.items.add (juce::FlexItem (soloTrackButton).withHeight (buttonHeight).withMargin (margin));
