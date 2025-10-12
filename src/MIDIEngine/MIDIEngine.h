4e8f27491a8510a91e1b9f12e8900775c501d43a
Tommy Shelburne <tshelburne02@gmail.com>
2025-10-12 14:53:39 -0600

    Fix new clips overlapping; default clip size 4 bars (from 8)

--- src/MIDIEngine/MIDIEngine.h
+++ src/MIDIEngine/MIDIEngine.h
@@ -2,38 +2,39 @@
 #pragma once
 
 #include "../AppEngine/TrackManager.h"
-#include <tracktion_engine/tracktion_engine.h>
-
 #include "tracktion_graph/tracktion_graph.h"
+#include <tracktion_engine/tracktion_engine.h>
 
 namespace te = tracktion;
 
 class MIDIEngine
 {
 public:
-    explicit MIDIEngine(tracktion::engine::Edit& editRef);
+    explicit MIDIEngine (tracktion::engine::Edit& editRef);
     ~MIDIEngine() = default;
 
     /**
-     * Adds a Midi clip to a specific track
+     * Adds a Midi clip to a specific track (Junie).
      * @param trackIndex the index of the track to add the clip to.
      */
-    void addMidiClipToTrack(int trackIndex);
+    void addMidiClipToTrack (int trackIndex) const;
 
     /**
-     * Returns the first MidiClip* from a track in the edit instance (or nullptr if none)
+     * Returns the first MidiClip* from a track in the edit instance (or nullptr if none) (Junie).
+     * @param trackIndex the index of the track to add the clip to.
      */
-    te::MidiClip* getMidiClipFromTrack(int trackIndex);
+    te::MidiClip* getMidiClipFromTrack (int trackIndex) const;
 
     /**
-     * Returns all MIDI clips on the given track.
+     * Returns all MIDI clips on the given track (Junie).
+     * @param trackIndex the index of the track to add the clip to.
      */
-    std::vector<te::MidiClip*> getMidiClipsFromTrack(int trackIndex);
+    std::vector<te::MidiClip*> getMidiClipsFromTrack (int trackIndex) const;
 
     int addMidiTrack();
 
 private:
     tracktion::engine::Edit& edit;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MIDIEngine)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MIDIEngine)
 };
