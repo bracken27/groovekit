4d9756ca9b5ed5a641b348018bc21fc75d9dac2c
bracken27 <brackenasay@gmail.com>
2025-11-18 23:26:39 -0700

    fixed bug in midi clip importing

--- src/AppEngine/AppEngine.cpp
+++ src/AppEngine/AppEngine.cpp
@@ -1400,46 +1400,6 @@ void AppEngine::importMidiClipViaChooser (int trackIndex,
     );
 }
 
-void AppEngine::importMidiClipViaChooser (int trackIndex,
-                                          t::TimePosition destStart,
-                                          std::function<void()> onSuccess)
-{
-    auto chooser = std::make_shared<juce::FileChooser> (
-        "Import MIDI File",
-        juce::File(),
-        "*.mid;*.midi"
-    );
-
-    chooser->launchAsync (
-        juce::FileBrowserComponent::openMode
-        | juce::FileBrowserComponent::canSelectFiles,
-        [this, chooser, trackIndex, destStart, onSuccess] (const juce::FileChooser& fc)
-        {
-            auto file = fc.getResult();
-
-            if (! file.existsAsFile())
-            {
-                return;
-            }
-
-            const bool ok = midiEngine->importMidiFileToTrack (file, trackIndex, destStart);
-
-            if (! ok)
-            {
-                juce::AlertWindow::showMessageBoxAsync (
-                    juce::AlertWindow::WarningIcon,
-                    "MIDI Import Failed",
-                    "Could not import the MIDI file into this track.");
-            }
-            else
-            {
-                if (onSuccess)
-                    onSuccess();
-            }
-        }
-    );
-}
-
 bool AppEngine::hasClipboardContent() const
 {
     const auto* cb = te::Clipboard::getInstance();
